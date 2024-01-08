960914bcb2a2a1f75c09c5e61517e97a9f59265e
Anant Dubey <anantkumar312@gmail.com>
2024-01-08 13:42:02 +0530

    feat: add custom widget

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import "package:flutter/material.dart";
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(),
+      ),
+    );
+  }
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.amber, Colors.amberAccent],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello World",
+          style: TextStyle(
+            color: Colors.brown,
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
+}
